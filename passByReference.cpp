dc92ef4c4f9c906ac663ffc2101ea5ddb46bb2df
Jiashuoli98 <3468034737@qq.com>
2021-04-02 17:02:14 +0800

    Add files via upload
    pass by value

---
+++ passByReference.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace  std;
+
+void swap(int x, int y) {
+	int t = 0;
+	t = x; x = y; y = t;
+	
+}
+int main() {
+	int a = 1, b = 22;
+	cout << "before calling swap(): a " << a << " b " << b<<endl;
+	swap(a, b);
+	cout << "after calling swap(): a " << a << " b " << b<<endl;
+
+
+
+
+	return 0;
+
+
+
+}
