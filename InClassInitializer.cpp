b989fca7bba68aaf45e313176e2a745c41433938
Jiashuoli98 <3468034737@qq.com>
2021-04-09 16:23:38 +0800

    Add files via upload

---
+++ InClassInitializer.cpp
@@ -0,0 +1,26 @@
+﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+class x {
+public:
+    int a = 1;
+    int b = { 2 };
+    int c{ 3 };
+    string s1{ 'H','E','L' };
+    string s2{ "HELLO" };
+    string s3 = { "world" };
+    int arr1[3] = { 1,2,3 };
+    int arr2[4]{ 1 };
+};
+int main()
+{
+    x x1;
+    cout << x1.s1 << endl;
+    cout << x1.s2 << endl;
+    cout << x1.s3 << endl;
+
+}
